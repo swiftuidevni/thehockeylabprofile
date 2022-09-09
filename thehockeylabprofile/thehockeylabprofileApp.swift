aaf1f6edc036dbe3a70c2bfaba0f1a3d3ef05bb9
swiftdevui <swiftuinidev@gmail.com>
2022-09-09 03:10:24 +0100

    Initial Commit

---
+++ thehockeylabprofile/thehockeylabprofileApp.swift
@@ -0,0 +1,17 @@
+//
+//  thehockeylabprofileApp.swift
+//  thehockeylabprofile
+//
+//  Created by David Buckley on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct thehockeylabprofileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
